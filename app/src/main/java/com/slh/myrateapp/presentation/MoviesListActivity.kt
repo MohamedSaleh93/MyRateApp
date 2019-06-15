40ef5a261d9f3a5743efb7139182776407a27c90
Mohamed saleh <mohamedsaleh.fcis@gmail.com>
2019-06-15 15:03:46 +0200

    initialize the home launcher activity of the app

---
+++ app/src/main/java/com/slh/myrateapp/presentation/MoviesListActivity.kt
@@ -0,0 +1,14 @@
+package com.slh.myrateapp.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.slh.myrateapp.R
+
+class MoviesListActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movies_list)
+    }
+
+}

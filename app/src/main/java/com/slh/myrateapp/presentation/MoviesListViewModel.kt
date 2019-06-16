e770751decbb188fd924395670cbd22dc6505b7c
Mohamed saleh <mohamedsaleh.fcis@gmail.com>
2019-06-16 22:04:42 +0200

    build the composite  disposable for MoviesListViewModel

--- app/src/main/java/com/slh/myrateapp/presentation/MoviesListViewModel.kt
+++ app/src/main/java/com/slh/myrateapp/presentation/MoviesListViewModel.kt
@@ -5,10 +5,13 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.slh.myrateapp.data.MoviesUseCase
 import com.slh.myrateapp.model.MovieModel
+import io.reactivex.disposables.CompositeDisposable
 
 class MoviesListViewModel(val moviesUseCase: MoviesUseCase): ViewModel() {
 
     val moviesListObservable = MutableLiveData<List<MovieModel>>()
+
+    private val compositeDisposable = CompositeDisposable()
 
     fun getMoviesList() {
 
@@ -22,6 +25,10 @@ class MoviesListViewModel(val moviesUseCase: MoviesUseCase): ViewModel() {
 
     }
 
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.dispose()
+    }
 
     class MoviesListViewModelFactory(val moviesUseCase: MoviesUseCase): ViewModelProvider.Factory {
         override fun <T : ViewModel?> create(modelClass: Class<T>): T {

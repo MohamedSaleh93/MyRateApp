287ea914f8677f2d5a967f321a39825a59047c39
Mohamed saleh <mohamedsaleh.fcis@gmail.com>
2019-06-16 22:15:13 +0200

    write test for getMoviesList in view model and it fails now .. so we need to write the implementation which make the test success

---
+++ app/src/test/java/com/slh/myrateapp/presentation/MoviesListViewModelTest.kt
@@ -0,0 +1,61 @@
+package com.slh.myrateapp.presentation
+
+import com.slh.myrateapp.data.MoviesUseCase
+import com.slh.myrateapp.factory.MoviesListFactoryImpl
+import com.slh.myrateapp.model.MovieModel
+import io.reactivex.Single
+import io.reactivex.android.plugins.RxAndroidPlugins
+import io.reactivex.plugins.RxJavaPlugins
+import io.reactivex.schedulers.Schedulers
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.Before
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+
+class MoviesListViewModelTest {
+
+    @Mock
+    lateinit var moviesUseCase: MoviesUseCase
+
+    lateinit var moviesViewMovieModel: MoviesListViewModel
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.initMocks(this)
+        RxJavaPlugins.setIoSchedulerHandler { Schedulers.trampoline() }
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler { Schedulers.trampoline() }
+        moviesViewMovieModel = MoviesListViewModel(moviesUseCase)
+    }
+    @Test
+    fun getMoviesList() {
+        Mockito.`when`(moviesUseCase.getMoviesList()).thenReturn(Single.just(buildMoviesMockList()))
+        moviesViewMovieModel.getMoviesList()
+        assertEquals(moviesViewMovieModel.moviesListObservable.value?.size, buildMoviesMockList().size)
+    }
+
+    @Test
+    fun rateMovie() {
+    }
+
+    @Test
+    fun randomRateMovies() {
+    }
+
+    private fun buildMoviesMockList(): List<MovieModel> {
+        val moviesList = ArrayList<MovieModel>()
+        moviesList.add(MovieModel("Inception", 5.0))
+        moviesList.add(MovieModel("Oldboy", 4.9))
+        moviesList.add(MovieModel("Bridge Of Spies", 4.8))
+        moviesList.add(MovieModel("The shawshank Redemption", 4.7))
+        moviesList.add(MovieModel("Intersteller", 4.5))
+        moviesList.add(MovieModel("Pulp fiction", 4.4))
+        moviesList.add(MovieModel("Kill Bill ", 4.3))
+        moviesList.add(MovieModel("The Good, The Bad and The Ugle", 3.5))
+        moviesList.add(MovieModel("The Message", 4.6))
+        moviesList.add(MovieModel("Get Out", 2.1))
+        return moviesList
+    }
+}
